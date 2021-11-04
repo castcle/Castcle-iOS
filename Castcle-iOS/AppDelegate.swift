30a61a6e5b110e4209ce17222b2dd514fa3f830b
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-11-04 16:39:28 +0700

    Handle view profile view

--- Castcle-iOS/AppDelegate.swift
+++ Castcle-iOS/AppDelegate.swift
@@ -95,9 +95,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // MARK: - Migrations Realm
         let config = Realm.Configuration(
-            schemaVersion: 2,
+            schemaVersion: 3,
             migrationBlock: { migration, oldSchemaVersion in
-                if (oldSchemaVersion < 2) {
+                if (oldSchemaVersion < 3) {
                     // Nothing to do!
                     // Realm will automatically detect new properties and removed properties
                     // And will update the schema on disk automatically
